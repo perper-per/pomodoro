4a04729c0432d86e19a38af7c9f1a0b00311cb01
perper-per <yes.its.wpw@gmail.com>
2024-12-11 13:25:54 +0800

    +GA

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -45,6 +46,12 @@ export default defineConfig({
       vueTemplate: true,
     }),
     vueDevTools(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-0B3R38TK16',
+      },
+    }),
   ],
   define: { 'process.env': {} },
   resolve: {
